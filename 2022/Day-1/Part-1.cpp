27fe3f4834b7e6107b179e53e35835a6bf577c48
Antarctic Oscillation <antarticoscillation@gmail.com>
2023-07-23 23:08:55 +0400

    update.

---
+++ 2022/Day-1/Part-1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream inputFile("input.txt");
+    std::ofstream outputFile("output.txt");
+    //-------------------------------------
+    int max_sum(0);
+    {
+        std::string line;
+        int sum(0);
+        while (std::getline(inputFile, line)) {
+            if (!line.empty()) {
+                sum += std::stoi(line);
+            } else {
+                max_sum = std::max(max_sum, sum);
+                sum = 0;
+            }
+        }
+        max_sum = std::max(max_sum, sum);
+    }
+
+    outputFile << max_sum;
+    //-------------------------------------
+    inputFile.close();
+    outputFile.close();
+    return 0;
+}
