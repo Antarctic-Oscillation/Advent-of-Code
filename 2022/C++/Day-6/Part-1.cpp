5da511da2b39b48551dcd6a1f28da4999c83695b
Antarctic Oscillation <antarticoscillation@gmail.com>
2023-08-11 11:05:39 +0400

    updated.

---
+++ 2022/C++/Day-6/Part-1.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <unordered_set>
+
+constexpr int MARKER_LENGTH = 4;
+
+bool hasUniqueElements(const std::string& str) {
+    std::unordered_set<char> encounteredElements;
+    for (char c : str) {
+        if (encounteredElements.count(c)) {
+            return false;
+        }
+        encounteredElements.insert(c);
+    }
+    return true;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::ifstream inputFile("input.txt");
+    //-------------------------------------
+    int answer(MARKER_LENGTH);
+
+    std::string line;
+    std::getline(inputFile, line);
+    std::string subStr = line.substr(0, MARKER_LENGTH);
+
+    if (!hasUniqueElements(subStr)) {
+        for (int i = MARKER_LENGTH, n = line.length(); i < n; ++i) {
+            subStr = subStr.substr(1) + line[i];
+            if (hasUniqueElements(subStr)) {
+                answer = i + 1;
+                break;
+            }
+        }
+    }
+
+    std::cout << answer << "\n";
+    //-------------------------------------
+    return 0;
+}
